a25d145540480eaf3f02e386c960e6ee0f091928
GMendz <guimendesjf@ymail.com>
2024-09-11 17:20:54 -0300

    tabuada

---
+++ dia7/dia7.py
@@ -0,0 +1,9 @@
+def tabuada(numero):
+ 
+  for i in range(1, 11):
+    resultado = numero * i
+    print(f"{numero} x {i} = {resultado}")
+
+numero = int(input("Digite um número para ver sua tabuada: "))
+
+tabuada(numero)
