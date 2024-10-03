668c2b6fb9e79e6632897aabbda72ea491ab69f4
GMendz <guimendesjf@ymail.com>
2024-10-03 19:02:17 -0300

    Gerador de Senhas Aleatórias

--- dia17/main.js
+++ dia17/main.js
@@ -1,28 +1,32 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Dia 17 - 100 Dias De Código</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+let sizePassword = document.querySelector(" valor");
+let password = document.querySelector("#password");
+let containerPassword = document.querySelector("#container-password");
 
-    <body>
-        <img src="logo.png" alt="logo do projeto" class="logo">
-        
-        <main class="container-input">
-            <span>Tamanho<span id="valor"></span>caracteres</span>
-            <input type="range" id="slidert" min="5" max="25" value="15">
+let charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz{}()!@#$%¨&*";
+let novaSenha = "";
 
-            <button id="button" class="button-cte" onclick=""></button>
-        </main>
+sizePassword.innerHTML = sliderElement.ariaValueMax;
 
-        <div class="container-password" onclick="">
-            <span class="title">Sua senha gerada é:</span>
-            <span id="password" class="password"></span>
-            <span class="tooltip">Clique na senha para copiar</span>
-        </div>
-        
-        <script src="main.js"></script>
-    </body>
-</html>
+sliderElement.oninput = function(){
+    sizePassword.innerHTML = this.value;
+}
+
+function gerarSenha(){
+
+    let senha = "";
+    for(let i = 0, n = charset.length; i < sliderElement.value; i++) {
+
+        senha += charset.charAt(Math.floor(Math.random() * n))
+    }
+
+    containerPassword.classList.remove("hide");
+
+    password.innerHTML = senha;
+}
+
+function copiarSenha() {
+    alert("Senha copiada com sucesso!");
+    navigator.clipboard.writeText(password);
+}
